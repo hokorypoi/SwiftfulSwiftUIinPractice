9a2e5b23e0221880ce042458738e050dc3210f3a
Nya <sntdawuk@qq.com>
2025-02-13 20:48:59 +0800

    Initial Commit

---
+++ SwiftfulSwiftUIinPractice/SwiftfulSwiftUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUIinPracticeApp.swift
+//  SwiftfulSwiftUIinPractice
+//
+//  Created by nya on 2025/2/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
