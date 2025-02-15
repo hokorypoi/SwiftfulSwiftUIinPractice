4c833292691f4ab22e5a3cdbc358a3093a8ffaca
Nya <sntdawuk@qq.com>
2025-02-15 11:00:06 +0800

    [Feature] Bumble Part(1/5)

---
+++ SwiftfulSwiftUIinPractice/Bumble/Components/BumbleFilterView.swift
@@ -0,0 +1,56 @@
+//
+//  BumbleFilterView.swift
+//  SwiftfulSwiftUIinPractice
+//
+//  Created by nya on 2025/2/15.
+//
+
+import SwiftUI
+
+struct BumbleFilterView: View {
+    
+    var options: [String] = ["Everyone", "Trending"]
+    @Binding var selection: String
+    @Namespace private var namespace
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 32.0) {
+            ForEach(options, id: \.self) { option in
+                VStack(spacing: 8.0) {
+                    Text(option)
+                        .frame(maxWidth: .infinity)
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                    
+                    if selection == option {
+                        RoundedRectangle(cornerRadius: 2)
+                            .frame(height: 1.5)
+                            .matchedGeometryEffect(id: "selection", in: namespace)
+                    }
+                }
+                .padding(.top, 8)
+                .background(Color.black.opacity(0.001))
+                .foregroundStyle(selection == option ? .bumbleBlack : .bumbleGray)
+                .onTapGesture {
+                    selection = option
+                }
+            }
+        }
+        .animation(.smooth, value: selection)
+    }
+}
+
+fileprivate struct BumbleFilterViewPreview: View {
+    
+    var options: [String] = ["Everyone", "Trending"]
+    @State private var selection = "Everyone"
+    
+    var body: some View {
+        BumbleFilterView(options: options, selection: $selection)
+    }
+}
+
+#Preview {
+    BumbleFilterViewPreview()
+        .padding()
+}
