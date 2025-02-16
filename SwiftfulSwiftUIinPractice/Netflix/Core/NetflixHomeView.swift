ef5558349c402d5898e67ce8582778e4ba4c0081
Nya <sntdawuk@qq.com>
2025-02-16 12:33:13 +0800

    [Feature] Netflix Path (3/6)

--- SwiftfulSwiftUIinPractice/Netflix/Core/NetflixHomeView.swift
+++ SwiftfulSwiftUIinPractice/Netflix/Core/NetflixHomeView.swift
@@ -6,15 +6,37 @@
 //
 
 import SwiftUI
+import SwiftfulUI
 
 struct NetflixHomeView: View {
     
     @State private var filters = FilterModel.mockArray
     @State private var selectedFilter: FilterModel? = nil
+    @State private var fullHeaderSize: CGSize = .zero
+    
+    @State private var heroProduct: Product? = nil
+    @State private var currentUser: User? = nil
+    @State private var productRows: [ProductRow] = []
     
     var body: some View {
-        ZStack {
+        ZStack(alignment: .top) {
             Color.netflixBlack.ignoresSafeArea()
+            
+            ScrollView(.vertical) {
+                VStack(spacing: 8) {
+                    
+                    Rectangle()
+                        .opacity(0)
+                        .frame(height: fullHeaderSize.height)
+                    
+                    if let heroProduct {
+                        heroCell(heroProduct: heroProduct)
+                    }
+                    
+                    categoryRows
+                }
+            }
+            .scrollIndicators(.hidden)
             
             VStack(spacing: 0) {
                 header
@@ -28,10 +50,39 @@ struct NetflixHomeView: View {
                         selectedFilter =  nil
                     }
                     .padding(.top, 16)
-                Spacer()
+            }
+            .background(.blue)
+            .readingFrame { frame in
+                fullHeaderSize = frame.size
             }
         }
         .foregroundStyle(.netflixWhite)
+        .task {
+            await getData()
+        }
+        .toolbarVisibility(.hidden, for: .navigationBar)
+    }
+    
+    private func getData() async {
+        guard productRows.isEmpty else { return }
+        do {
+            currentUser = try await DatabaseHelper().getUsers().first
+            let products = try await DatabaseHelper().getProducts()
+            heroProduct = products.first
+            var rows: [ProductRow] = []
+            
+            let allBrands = Set(products.map({ $0.brand }))
+            for brand in allBrands {
+                if let brand {
+//                    let products = self.products.filter({ $0.brand == brand })
+                    rows.append(ProductRow(title: brand.capitalized, products: products))
+                }
+            }
+            productRows = rows
+            
+        } catch {
+            print("Error: \(error)")
+        }
     }
     
     private var header: some View {
@@ -50,6 +101,52 @@ struct NetflixHomeView: View {
                     }
             }
             .font(.title)
+        }
+    }
+    
+    private func heroCell(heroProduct: Product) -> some View {
+        NetflixHeroCell(
+            imageName: heroProduct.firstImage,
+            isNetflixFilm: true,
+            title: heroProduct.title,
+            categories: [heroProduct.category?.capitalized ?? "C", heroProduct.brand ?? "B"],
+            onBackgroundPressed: {
+                
+            },
+            onPlayPressed: {
+                
+            },
+            onMyListPressed: {
+                
+            }
+        )
+        .padding(24)
+    }
+    
+    private var categoryRows: some View {
+        LazyVStack(spacing: 16) {
+            ForEach(Array(productRows.enumerated()), id: \.offset) { (rowIndex, row) in
+                VStack(alignment: .leading, spacing: 6) {
+                    Text(row.title)
+                        .font(.headline)
+                        .padding(.horizontal, 16)
+                    
+                    ScrollView(.horizontal) {
+                        LazyHStack {
+                            ForEach(Array(row.products.enumerated()), id: \.offset) { (index, product) in
+                                NetflixMovieCell(
+                                    imageName: product.firstImage,
+                                    title: product.title,
+                                    isRecentlyAdded: product.recentlyAdded,
+                                    topTenRanking: rowIndex == 1 ? (index + 1) : nil
+                                )
+                            }
+                        }
+                        .padding(.horizontal, 16)
+                    }
+                    .scrollIndicators(.hidden)
+                }
+            }
         }
     }
 }
